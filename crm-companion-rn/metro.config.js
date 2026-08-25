c6ac22a326c348c7617bff282b68dd5aeee63579
Aakash's Mac <apple@Apples-MacBook-Air.local>
2026-08-26 02:48:41 +0530

    fix: remove restrictive blockList in metro.config.js to allow Metro SHA-1 resolution of nested expo modules

--- crm-companion-rn/metro.config.js
+++ crm-companion-rn/metro.config.js
@@ -1,12 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-// Exclude parent project node_modules from file indexing on Windows
-config.resolver.blockList = [
-  /node_modules\/.*\/node_modules/,
-  /dazzling-bose[\\/]node_modules/
-];
 
 module.exports = config;
